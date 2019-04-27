25b7ce02c358a769753a9e9cb8238b6a46f8c2d3
Jose Troche <jose.troche.coder@gmail.com>
2019-04-27 10:00:38 -0400

    Create install_ffmpeg_for_mac.sh

---
+++ install_ffmpeg_for_mac.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Installs ffmpeg in a Mac. But you can also install ffmpeg in 
+# Windows (http://ffmpeg.org/download.html#build-windows) and
+# Linux (http://ffmpeg.org/download.html#build-linux)
+
+# Prerequisites for Mac
+# xcode and homebrew. Uncomment the lines below if you need those
+
+# xcode
+# xcode-select --install
+
+# Homebrew
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# ffmpeg
+brew install ffmpeg
